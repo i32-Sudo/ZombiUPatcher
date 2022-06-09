93d715bd54079440e6aa2637f2a879c2dee9be20
Ezekial/Zeke <32526701+Zurek0x@users.noreply.github.com>
2022-06-08 20:24:42 -0700

    Update ZombiU_XPatcher.cpp

--- dev_patcher-BASE/ZombiU_XPatcher.cpp
+++ dev_patcher-BASE/ZombiU_XPatcher.cpp
@@ -1,3 +1,9 @@
+// Ignore this, This is developer issued code and has nothing to do with patching the game
+// This code is for the older version of the Patcher (v1.3 and below) So ignore this.
+//
+// We updated it to (v1.4+) and now only has the modded game files and doesn't require any external code
+// You can ignore this.
+
 #include <windows.h>
 #include <iostream>
 #include <thread>
