19b480ed9bdf91538b27cfd77be88e9e227199ef
Ezekial/Zeke <32526701+Zurek0x@users.noreply.github.com>
2022-06-08 20:25:01 -0700

    Update Program.cs

--- dev_overlay-BASE/Program.cs
+++ dev_overlay-BASE/Program.cs
@@ -1,3 +1,10 @@
+// Ignore this, This is developer issued code and has nothing to do with patching the game
+// This code is for the older version of the Patcher (v1.3 and below) So ignore this.
+//
+// We updated it to (v1.4+) and now only has the modded game files and doesn't require any external code
+// You can ignore this.
+
+
 namespace ZombiU_XPatcherGUI
 {
     internal static class Program
